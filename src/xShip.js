543141dc509d633879378d28c727afe7abfa288c
Artur Kot <arturkot85@gmail.com>
2016-11-22 16:31:43 +0100

    Add phong material for xShip

--- src/xShip.js
+++ src/xShip.js
@@ -1,6 +1,6 @@
 export function addXShip (scene, y) {
   const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
   const xShip = new THREE.Mesh(geometry, material);
   xShip.position.y = y;
   scene.add(xShip);
