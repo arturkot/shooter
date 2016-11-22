d0e3c35d5693446bc14cdf48257907be99d7533a
Artur Kot <arturkot85@gmail.com>
2016-11-22 23:52:12 +0100

    Use more readable numbers for RATIO

--- src/boundries.js
+++ src/boundries.js
@@ -1,5 +1,5 @@
 export const GAME_HEIGHT = 15;
-export const RATIO = 0.5625;
+export const RATIO = 9 / 16;
 export const gameWidth = GAME_HEIGHT * RATIO;
 export const leftBoundry = (gameWidth / -2);
 export const rightBoundry = (gameWidth / 2);
