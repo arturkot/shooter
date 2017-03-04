3918400895c23f87fc5833113e3106f74938af8b
Artur Kot <arturkot85@gmail.com>
2017-03-04 23:21:24 +0100

    Begin to work on enemies tests

---
+++ __tests__/enemies.ts
@@ -0,0 +1,15 @@
+import {
+  getDefaultEnemy,
+  updateEnemy
+} from '../src/enemies';
+
+describe('when enemy is updated', () => {
+  it(`shouldn't affect the previous enemy state`, () => {
+    const enemy = getDefaultEnemy(1);
+    const enemyClone = Object.assign({}, enemy);
+
+    updateEnemy(enemy, 99);
+
+    expect(enemy).toEqual(enemyClone)
+  });
+});
